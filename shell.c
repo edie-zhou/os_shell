14b0797e9b14a80a7cd995fbdd9081a7d9cb204d
Edie Zhou <edie198@gmail.com>
2019-08-29 19:05:18 -0500

    [shell] Initial commit for shell

---
+++ shell.c
@@ -0,0 +1,29 @@
+#include <stdio.h>​
+// #include​ <string.h>​
+#include​ <stdlib.h>​
+
+bool read(char* input[]){
+  char str[2000];
+
+  printf("# ");
+  gets(str);
+
+  return str;
+}
+
+// void parse(){
+//
+// }
+//
+// void execute(){
+//
+// }
+
+int main (void){
+  while(1){
+    char input[] = "\n"
+    read(input)
+    // parse()
+  }
+  return 0;
+})
