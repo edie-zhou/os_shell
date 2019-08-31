25530f95545ae1dd54dc09e68b3dfe3205bb5bbe
Edie Zhou <edie198@gmail.com>
2019-08-30 19:17:31 -0500

    Refactored parse and read out of main

--- test.c
+++ test.c
@@ -1,19 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main (void){
-  printf("Hello, world!\n");
-
-  char line[2000];
-
+/**
+ * Purpose:
+ *   Read in a line of input from stdin and verify that input line is valid
+ * 
+ * Args:
+ *   input      (char*): Pointer to input c-string
+ *   maxInputLen  (int): Maximum length of input
+ * 
+ * Returns:
+ *   (int): Returns 1 if string is valid, 0 if string is invalid
+ */
+int read(char input[], int maxInputLen){
+  const char LINE_FEED = 0x0A;
+  char line[maxInputLen];
   if(fgets(line, sizeof line, stdin) != NULL){
-    /* Now inspect and further parse the string in line. */
     int k = 0;
-    char nullTerm = '\0';
-    while(line[k] != nullTerm){
-      printf("%c", line[k]);
+    while((line[k] != LINE_FEED) && (k < maxInputLen)){
+      strcpy(input, line);
       k++;
     }
+    return 1;
   }
+  else{
+    return 0;
+  }
+}
+
+/**
+ * Purpose:
+ *   Parse input line and check if commands are valid
+ * 
+ * Args:
+ *   input (char*): Pointer to input c-string
+ *  
+ * Returns:
+ *   (int): Returns 1 is string is valid, 0 if string is invalid
+ * 
+ */
+int parse (char input[]){
+  int k = 0;
+  const char LINE_FEED = 0x0A;
+  while(input[k] != LINE_FEED){
+    printf("%c", input[k]);
+    k++;
+  }
+}
+
+/**
+ * Purpose:
+ *   Execute input commands from command line
+ * 
+ * Args:
+ *   input (char*): Pointer to input c-string
+ * 
+ * Returns:
+ * 
+ */
+void execute(char* input[]){
+
+}
+
+int main (void){
+  const char NEW_LINE = 0x0A;
+  const int MAX_LINE_LEN = 2000;
+  int validInput;
+  int validParse;
+  char* input = (char*)malloc(MAX_LINE_LEN * sizeof(char));
+  validInput = read(input, MAX_LINE_LEN);
+  validParse = parse(input);
+  
+  free(input); 
   return 0;
 }
 
