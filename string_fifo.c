9af4b58ade1ab6a2f4da048a408d9fdfed554fe5
Edie Zhou <edie198@gmail.com>
2019-09-01 10:20:25 -0500

    Changed string_fifo.c to implement FIFO queue instead of LIFO stack

--- string_fifo.c
+++ string_fifo.c
@@ -8,41 +8,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**Pixel data structure
- * row: Row number of pixel
- * col: Column number of pixel
- * color: Char that represents color of pixel
+/**
+ * Purpose: Struct for FIFO queue node
+ * *next: Pointer to next node in queue
  */
-typedef struct Pixel {
-  int row;
-  int col;
-  char color;
-} Pixel;
+typedef struct QNode {
+  tokenSize = 30;
+  char* token;
+  struct QNode *next;
+} QNode;
 
-typedef Pixel StackEntry;
-
-/**StackNode data structure
- * StackEntry: Pixel stored at node
- * *next: Pointer to next node in linked list
+/**
+ * Queue data structure
+ * *top: Pointer to top of queue
  */
-typedef struct StackNode {
-  StackEntry pixel;
-  struct StackNode *next;
-} StackNode;
+typedef struct Queue {
+  struct QNode *top;
+} Queue;
 
-/**Stack data structure
- * *top: Pointer to top of linked list
+/**
+ * Purpose:
+ *   Create queue in heap
+ * 
+ * Args:
+ *   queue (Queue*): Pointer to head of queue
+ * 
+ * Returns:
+ *   None
  */
-typedef struct Stack312 {
-  struct StackNode *top;
-} Stack312;
-
-/**@Precondition Accepts a pointer as input parameter
- * @Postcondition Creates a linked list in the heap
- * @param *s Pointer to head of linked list
- */
-void makeStack(Stack312 *s){
-  s->top = NULL;
+void makeQueue(Queue* queue){
+  queue->top = NULL;
 }
 
 /**@Precondition Accepts a node as input parameter
@@ -50,46 +45,86 @@ void makeStack(Stack312 *s){
  * @param s Head of linked list
  * @return Boolean result checking if stack is full
  */
-int isFull(Stack312 s){
-  return false;
+int isFull(Queue s){
+  return 0;
 }
 
-/**@Precondition Accepts a node as input parameter
- * @Postcondition Checks if stack is full and returns boolean result
- * @param s Head of linked list
- * @return Boolean result checking if stack is empty
+/**
+ * Purpose:
+ *   Check if queue is empty
+ * 
+ * Args:
+ *   queue (Queue): Queue object to check for emptiness
+ * 
+ * Returns:
+ *   (int): 1 if empty, 0 if not empty
  */
-int isEmpty(Stack312 s){
+int isEmpty(Queue queue){
   int result = 0;
-  if(s.top == NULL){
+  if(queue.top == NULL){
     result = 1;
   }
   return result;
 }
 
-/**@Precondition Accepts a StackEntry and pointer as input parameters
- * @Postcondition Pushes StackEntry on to stack
- * @param e StackEntry to be pushed on to stack
- * @param *s Pointer to head of linked list
+/**
+ * Purpose:
+ *   Create and push first node to queue
+ * 
+ * Args:
+ *   node    (QNode): Node to be pushed to queue
+ *   queue  (Queue*): Pointer to queue object
+ *   cString (char*): Pointer to C-string
+ * 
+ * Returns:
+ *   None
  */
-void push(StackEntry e,Stack312 *s){
+void pushFirst(QNode node, Queue* queue, char* cString){
   struct StackNode* new = (struct StackNode*) malloc(sizeof(StackNode));
-  new->pixel = e;
+  new->token = cString;
   new->next = s->top;
   s->top = new;
 }
 
-/**@Precondition Accepts a pointer as input parameter
- * @Postcondition Returns popped entry from stack
- * @param *s Pointer to head of linked list
- * @return StackEntry that is popped off the stack
+/**
+ * Purpose:
+ *   Create and push nodes other than first node to queue
+ * 
+ * Args:
+ *   node    (QNode): Node to be pushed to queue
+ *   queue  (Queue*): Pointer to queue object
+ *   cString (char*): Pointer to C-string
+ * 
+ * Returns:
+ *   None
  */
-StackEntry pop(Stack312 *s){
-  StackEntry entry;
-  StackNode* temp;
-  temp = s->top;
-  s->top = temp->next;
+void push(QNode node, Queue* queue, char* cString){
+  struct StackNode* new = (struct StackNode*) malloc(sizeof(StackNode));
+  new->token = cString;
+  new->next = NULL;
+  QNode* nodePtr = queue->top;
+  while(nodePtr->next != NULL){
+    nodePtr = nodePtr->next;
+  }
+  nodePtr->next = new;
+}
+
+/**
+ * Purpose:
+ *   Remove and return node from queue
+ * 
+ * Args:
+ *   queue (Queue*): Pointer to queue
+ * 
+ * Returns
+ *   (char*): Pointer to C-string
+ */
+QNode pop(Queue* queue){
+  QNode entry;
+  Queue* temp;
+  temp = queue->top;
+  queue->top = temp->next;
   entry = temp->pixel;
   free(temp);
-  return entry;
+  return entry->token;
 }
