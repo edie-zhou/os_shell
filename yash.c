88ae38764eda5fdcec8357c27fb31aa2e9d2ac71
Edie Zhou <edie198@gmail.com>
2019-09-09 00:03:17 -0500

    Completed file redirection in execute function, fixed incorrect pointers in changeRedirToks()

--- yash.c
+++ yash.c
@@ -1,14 +1,16 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
 // Directions here:
-// TODO: https://docs.google.com/document/d/1LBMJslvYvw59uZ_8DNiiPzsp0heW3qesaalOo31IGYg/edit
+// https://docs.google.com/document/d/1LBMJslvYvw59uZ_8DNiiPzsp0heW3qesaalOo31IGYg/edit
 
 /** 
  * Purpose:
@@ -55,6 +57,8 @@ int checkTokens(char* input, int maxLineLen, int maxTokenLen){
  *   (int): Returns 1 if string is valid, 0 if string is invalid
  */
 int checkInput(char* input, int maxLineLen, int maxTokenLen){
+  // TODO: Add function to ensure file redir redirects to a file e.g.:# cat hello.txt >
+  // TODO: Add function to ensure pipe pipes to a valid command e.g.:# ls |
   if(strlen(input) > maxLineLen + 1){
     return 0;
   }
@@ -136,41 +140,6 @@ char** createTokenArray(char* input, int numTokens, int maxLineLen,
   return tokenArray;
 }
 
-// see fig 5.4 in OSTEP
-// TODO: Rewrite this stub to be used when file redirection is used
-#include <fcntl.h>
-#include <sys/stat.h>
-int executeFileRedirect(int argc, char *argv[]) {
-  int rc = fork();
-  if(rc < 0){
-    // fork failed; exit
-    fprintf(stderr, "fork failed\n");
-    exit(1);
-  }
-  else if(rc == 0){ // child: redirect standard output to a file
-    close(STDOUT_FILENO);
-    open("./p4.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
-    // now exec "wc"...
-    char *myargs[3];
-    myargs[0] = strdup("wc");
-    myargs[1] = strdup("p4.c");
-    myargs[2] = NULL;
-    execvp(myargs[0], myargs);
-  }
-  else{
-    // parent
-    int rc_wait = wait(NULL);
-  }
-  return 0;
-  // program: "wc" (word count)
-  // argument: file to count
-  // marks end of array
-  // runs word count
-}
-// TODO: Write execute function that handles piped i/o
-// TODO: Write execute handler function that decides which execute function to use,
-//       consider using indices of tokenArray
-
 /**
  * Purpose:
  *   Execute general input commands from command line
@@ -186,8 +155,8 @@ void execGeneral(char** tokenArray){
   int child = fork();
   if (child < 0) {
     // fork failed; exit
-    fprintf(stderr, "fork failed\n");
-    exit(1);
+    fprintf(stderr, "Fork failed\n");
+    exit(EXIT_FAILURE);
   }
   else if (child == 0) {
     // child (new process)
@@ -196,7 +165,7 @@ void execGeneral(char** tokenArray){
     // print new line if execvp fails (==-1) and exit process
     // these lines will not run unless execvp has failed
     printf("%c", NEW_LINE);
-    exit(1);
+    exit(EXIT_FAILURE);
   }
   else {
     // parent goes down this path (main)
@@ -209,7 +178,7 @@ void execGeneral(char** tokenArray){
 /**
  * Purpose:
  *   Find indices for input, output, and error redirection in a command and
- *   replace tokens will NULL
+ *   replace tokens with NULL
  * 
  * Args:
  *   tokenArray (char**): Array of tokens from command input
@@ -220,35 +189,27 @@ void execGeneral(char** tokenArray){
  * Returns:
  *   None  
  */ 
-void changeRedirIndices(char** tokenArray, int* inIndex, int* outIndex, int* errIndex){
-  const char NEW_LINE = '\n';
+void changeRedirToks(char** tokenArray, int* inIndex, int* outIndex,
+                     int* errIndex){
   const char* IN_REDIR = "<";
   const char* OUT_REDIR = ">";
   const char* ERR_REDIR = ">>";
 
   int index = 0;
   while(tokenArray[index] != NULL){
-    if((tokenArray[index] == IN_REDIR) &&
-       (tokenArray[index + 1] != NULL)){
-      inIndex = index + 1;
+    if(tokenArray[index] == IN_REDIR){
+      *inIndex = index + 1;
       // Assign NULL to stop exec() from reading file redirection as part of
       // input
       tokenArray[index] = NULL;
     }
-    else if((tokenArray[index] == OUT_REDIR) &&
-            (tokenArray[index + 1] != NULL)){
-      outIndex = index + 1;
+    else if(tokenArray[index] == OUT_REDIR){
+      *outIndex = index + 1;
       tokenArray[index] = NULL;
     }
-    else if((tokenArray[index] == ERR_REDIR) &&
-            (tokenArray[index + 1] != NULL)){
-      errIndex = index + 1;
+    else if(tokenArray[index] == ERR_REDIR){
+      *errIndex = index + 1;
       tokenArray[index] = NULL;
-    }
-    else if(tokenArray[index + 1] != NULL){
-      // ERROR: redirect with no target file
-      printf("%c", NEW_LINE);
-      return;
     }
     index++;
   }
@@ -266,29 +227,61 @@ void changeRedirIndices(char** tokenArray, int* inIndex, int* outIndex, int* err
  * Returns:
  *   None
  */
-void execRedirect(char** tokenArray){
+void execute(char** tokenArray){
+  // TODO: Finish this function
+  const int INVALID = -1;
   const char NEW_LINE = '\n';
-  const int VALID_INPUT = 1;
   int inIndex = -1;
   int outIndex = -1;
   int errIndex = -1;
+  int fdIn;
+  int fdOut;
 
-  changeRedirIndices(tokenArray, inIndex, outIndex, errIndex);
+  changeRedirToks(tokenArray, &inIndex, &outIndex, &errIndex);
 
   int child = fork();
   if (child < 0) {
     // fork failed; exit
-    fprintf(stderr, "fork failed\n");
-    exit(1);
+    fprintf(stderr, "Fork failed\n");
+    exit(EXIT_FAILURE);
   }
   else if (child == 0) {
+    if(inIndex != INVALID){
+      close(STDIN_FILENO);
+      if((fdIn = open(tokenArray[inIndex], O_RDONLY, 0)) == INVALID){
+          perror(tokenArray[inIndex]);
+          exit(EXIT_FAILURE);
+      }
+      dup2(fdIn, STDIN_FILENO);
+      close(fdIn);
+    }
+    if(outIndex != INVALID){
+      close(STDOUT_FILENO);
+      if((fdOut = open(tokenArray[outIndex], O_CREAT | O_WRONLY | O_TRUNC,
+          S_IRUSR | S_IRUSR | S_IRGRP | S_IROTH)) == INVALID){ 
+        perror(tokenArray[outIndex]);
+        exit(EXIT_FAILURE);
+      }
+      dup2(fdOut, STDOUT_FILENO);
+      close(fdOut);
+    }
+    if(errIndex != INVALID){
+      close(STDERR_FILENO);
+      if((fdOut = open(tokenArray[errIndex], O_CREAT | O_WRONLY | O_TRUNC,
+          S_IRUSR | S_IRUSR | S_IRGRP | S_IROTH)) == INVALID){ 
+        perror(tokenArray[errIndex]);
+        exit(EXIT_FAILURE);
+      }
+      dup2(fdOut, STDERR_FILENO);
+      close(fdOut);
+    }
     // child (new process)
     execvp(tokenArray[0], tokenArray);
 
     // print new line if execvp fails (==-1) and exit process
-    // these lines will not run unless execvp has failed
+    // these lines will not run unless execvp() has failed
     printf("%c", NEW_LINE);
-    exit(1);
+    exit(EXIT_FAILURE);
   }
   else {
     // parent goes down this path (main)
@@ -330,7 +323,7 @@ void checkJobControl(char** tokenArray, int numTokens){
     return;
   }
   else{
-    execGeneral(tokenArray);
+    execute(tokenArray);
     return;
   }
 }
@@ -359,7 +352,8 @@ void shellLoop(void){
     if(validInput){
 
       numTokens = countTokens(input, MAX_LINE_LEN);
-      char** tokenArray = createTokenArray(input, numTokens, MAX_LINE_LEN, MAX_TOKEN_LEN);
+      char** tokenArray = createTokenArray(input, numTokens, MAX_LINE_LEN,
+                                           MAX_TOKEN_LEN);
 
       checkJobControl(tokenArray, numTokens);
 
